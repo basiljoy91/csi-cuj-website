56d05a02828ff469107264712e89c57dd6e6d964
hemantprakash2005 <hemantprakash2005@gmail.com>
2025-08-04 19:58:43 +0530

    csi, cuj image size changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,35 +6,31 @@ import csiLogo from '../assets/csi_logo.png';
 const Header = () => {
   return (
     <header className="text-white py-4 shadow">
-      <div className="bg-white rounded-lg shadow-md p-4 flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0 md:space-x-8 mx-auto max-w-7xl">
-        {/* University Logo */}
-        <div className="flex justify-center md:justify-start w-full md:w-auto">
-          <img
-            src={universityLogo}
-            alt="University Logo"
-            className="h-28 md:h-32 w-auto object-contain"
-          />
-        </div>
+      <div className="bg-white rounded-lg shadow-md px-4 py-6 flex flex-row items-center justify-between space-x-2 sm:space-x-4 mx-auto">
+        
+        {/* Left: University Logo */}
+        <img
+          src={universityLogo}
+          alt="University Logo"
+          className="h-12 sm:h-20 lg:h-32 w-auto object-contain flex-shrink-0"
+        />
 
-        {/* Center Text */}
-        <div className="text-center">
-          <h1 className="text-csi-blue text-xl sm:text-2xl md:text-4xl lg:text-5xl font-extrabold leading-snug">
-            Computer Society Of India
-            <br />
-            Student Chapter
-            <br />
-            Central University of Jharkhand
-          </h1>
-        </div>
+        {/* Center: Text */}
+        <h1 className="text-csi-blue text-sm sm:text-base md:text-2xl lg:text-4xl font-extrabold text-center leading-tight flex-1 px-2">
 
-        {/* CSI Logo */}
-        <div className="flex justify-center md:justify-end w-full md:w-auto">
-          <img
-            src={csiLogo}
-            alt="CSI Logo"
-            className="h-28 md:h-32 w-auto object-contain"
-          />
-        </div>
+          Computer Society Of India,
+          <br />
+          Student Chapter,
+          <br />
+          Central University of Jharkhand, Ranchi
+        </h1>
+
+        {/* Right: CSI Logo */}
+        <img
+          src={csiLogo}
+          alt="CSI Logo"
+          className="h-12 sm:h-20 lg:h-32 w-auto object-contain flex-shrink-0"
+        />
       </div>
     </header>
   );
